703bca4eed8c7b61c356a8db81511018e1086d9f
Tony Klinakis <a.klinakis@yahoo.com>
2025-01-09 22:17:34 +0000

    #3 Test version and add prettier

---
+++ src/__tests__/version.test.ts
@@ -0,0 +1,14 @@
+import { execSync } from 'child_process';
+
+function exec(command: string) {
+	return execSync(command, { encoding: 'utf-8' });
+}
+
+
+describe('version', () => {
+	it('should print the current version when invoked with the "version" command', function() {
+		const stdout = exec('npx . version').trim();
+
+		expect(stdout).toEqual('VERSION_REPLACE');
+	});
+});
