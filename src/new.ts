a9e7439bc9776c26935696290a43e64060de1492
Tony Klinakis <a.klinakis@yahoo.com>
2025-01-09 22:21:56 +0000

    Refactor createRecord to make it more testable

--- src/new.ts
+++ src/new.ts
@@ -1,23 +1,39 @@
-import fs from "fs";
-import {countRecords, formatDate, formatIndex, genFileName, joinArrayIntoString, readFolderLocation} from "./util";
-import {DECISION_TEMPLATE} from "./templates";
-import {spawnSync} from "child_process";
+import fs from 'fs';
+import { countRecords, formatDate, formatIndex, genFileName, joinArrayIntoString, readFolderLocation } from './util';
+import { DECISION_TEMPLATE } from './templates';
+import { spawnSync } from 'child_process';
+
+type DecisionDocument = {
+	index: number;
+	filename: string;
+	title: string;
+}
+
+export function _createRecord(argv: string[], adrLocation: string): DecisionDocument {
+	const index = countRecords(adrLocation) + 1;
+	const title = joinArrayIntoString(argv);
+	const filename = genFileName(index, title);
+
+	return {
+		index,
+		title,
+		filename,
+	};
+}
 
 export function createRecord() {
 	const adrLocation = readFolderLocation();
 
-	const index = countRecords(adrLocation) + 1;
-	const title = joinArrayIntoString(process.argv);
-	const filename = genFileName(index, title);
+	const { index, title, filename } = _createRecord(process.argv, adrLocation);
 
-	fs.writeFileSync(`${adrLocation}/${filename}`, compileTemplate(index, title), {encoding: "utf-8"})
+	fs.writeFileSync(`${adrLocation}/${filename}`, compileTemplate(index, title), { encoding: 'utf-8' });
 
-	spawnSync(`eval $EDITOR ${adrLocation}/${filename}`)
+	spawnSync(`eval $EDITOR ${adrLocation}/${filename}`);
 }
 
 function compileTemplate(index: number, title: string, date = new Date()) {
 	return DECISION_TEMPLATE
-		.replace("{{NUMBER}}", formatIndex(index))
-		.replace("{{TITLE}}", title)
-		.replace("{{DATE}}", formatDate(date));
+		.replace('{{NUMBER}}', formatIndex(index))
+		.replace('{{TITLE}}', title)
+		.replace('{{DATE}}', formatDate(date));
 }
